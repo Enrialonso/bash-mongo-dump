0243eb150b20c4ce3301f7d2b2e84fe06def9546
proclamer <enrialonso@gmail.com>
2021-02-06 21:48:43 +0100

    primer commit

--- mongodump-kundall.sh
+++ mongodump-kundall.sh
@@ -5,6 +5,6 @@ export DB_NAME=kundall_db
 
 mongodump --forceTableScan -d ${DB_NAME} -o ${DUMP_FOLDER}${FILE_NAME}
 
-aws s3 cp ${DUMP_FOLDER}${FILE_NAME} s3://kundall-backup/mongo/ --recursive
+aws s3 cp ${DUMP_FOLDER} s3://kundall-backup/mongo/ --recursive
 
-# rm -fr ${DUMP_FOLDER}${FILE_NAME}
+rm -fr ${DUMP_FOLDER}${FILE_NAME}
