45748b8539f45921d1d06e0056b443e92046473a
proclamer <enrialonso@gmail.com>
2021-02-06 21:19:38 +0100

    primer commit

--- mongodump-kundall.sh
+++ mongodump-kundall.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -ex
 export DUMP_FOLDER=/home/ubuntu/respaldos/
-# export DUMP_FOLDER=/home/enrique/Documentos/respaldos/
 export FILE_NAME=kundall-backup-$(date +"%m-%d-%y_at_%H-%M")
 export DB_NAME=kundall_db
-# export DB_NAME=kundall
 
 mongodump --forceTableScan -d ${DB_NAME} -o ${DUMP_FOLDER}${FILE_NAME}
 
